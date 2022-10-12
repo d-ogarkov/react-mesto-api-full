e0de3f5b4854139bccf8f1b65cee158e7cb2ae95
Dmitry Ogarkov <dima@ogarkov.com>
2022-10-12 19:04:26 +0300

    15-я проектная работа, предварительная проверка

--- backend/constants/cors.js
+++ backend/constants/cors.js
@@ -5,7 +5,7 @@ const allowedCors = [
 
 const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
-const DEFAULT_ALLOWED_HEADERS = 'Origin, X-Requested-With, Content-Type, Accept';
+const DEFAULT_ALLOWED_HEADERS = 'Origin, X-Requested-With, Content-Type, Accept, Authorization';
 
 module.exports = {
   allowedCors, DEFAULT_ALLOWED_METHODS, DEFAULT_ALLOWED_HEADERS,
